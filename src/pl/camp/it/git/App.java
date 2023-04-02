3f9d4dc410d272a640a5b0a635634dc60c29fb93
Mateusz <bmatteo@interia.pl>
2023-04-02 16:54:40 +0200

    Jakas zmiana

--- src/pl/camp/it/git/App.java
+++ src/pl/camp/it/git/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Czesc");
         System.out.println("Jestem pan git !!");
+        System.out.println("cos");
     }
 }
