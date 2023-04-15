554f44961d7b4219ab2e97ebbe71a100b32db122
Mateusz <mat.bereda@gmail.com>
2023-04-15 09:24:42 +0200

    Zmiana

--- src/pl/camp/it/git/App.java
+++ src/pl/camp/it/git/App.java
@@ -5,5 +5,7 @@ public class App {
         System.out.println("Czesc");
         System.out.println("Jestem pan git !!");
         System.out.println("cos");
+
+        System.out.println("Cos nowego !!");
     }
 }
